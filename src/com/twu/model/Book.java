b0f00e8b5673ec2d1810077c6aece592ed3f1b44
mihe <mihe@thoughtworks.com>
2017-09-05 14:46:06 +0800

    book list

---
+++ src/com/twu/model/Book.java
@@ -0,0 +1,11 @@
+package com.twu.model;
+
+public class Book {
+    String name;
+    public Book(String name){
+        this.name = name;
+    }
+    public String toString(){
+        return name+"\n";
+    }
+}
